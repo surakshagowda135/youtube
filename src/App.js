10d0d89f9aa9dddf9f50ac515bc9687139d00434
suraksha135 <surakshagowda135@gmail.com>
2023-05-29 20:58:23 +0530

    add navbar componnt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Component/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1> Cloning YouTube</h1>
+      <h1 > Cloning YouTube</h1>
+      <Navbar/>
      
     </div>
   );
---
+++ src/Component/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <div className="Navbar">
+            <h1>NavBar</h1>
+        </div>
+    );
+
+}
+export default Navbar;
